3e86af20381f55b5b1f216a5e80dfe47e998795d
Sam Lui <asurada98@gmail.com>
2022-03-27 12:23:55 +0100

    Updates

--- js/menu.js
+++ js/menu.js
@@ -14,26 +14,33 @@ menuButton.addEventListener("click", () => {
 });
 
 //Scroll Event
-// window.sr = ScrollReveal();
+ window.sr = ScrollReveal();
 
-// sr.reveal('.animate-left',{
-//   origin:'left',
-//   duration:1000,
-//   distance:'25rem',
-//   delay: 300  
-// });
+sr.reveal('.animate-left',{
+origin:'left',
+duration:1000,
+distance:'25rem',
+delay: 300  
+});
 
-// sr.reveal('.animate-right',{
-//     origin:'right',
-//     duration:1000,
-//     distance:'25rem',
-//     delay: 600  
-//   });
+sr.reveal('.animate-right',{
+    origin:'right',
+    duration:1000,
+    distance:'25rem',
+    delay: 600  
+});
 
 
-//   sr.reveal('.animate-up',{
-//     origin:'top',
-//     duration:1000,
-//     distance:'25rem',
-//     delay: 600  
-//   });
+sr.reveal('.animate-up',{
+    origin:'top',
+    duration:1000,
+    distance:'25rem',
+    delay: 600  
+});
+
+sr.reveal('.animate-bottom',{
+    origin:'bottom',
+    duration:1000,
+    distance:'25rem',
+    delay: 600  
+});
